3ac0a0062a9343b030bc34bd563923b75a15167c
Lawa0921 <bag571ivy3470@gmail.com>
2020-04-21 11:13:59 +0800

    製作 show 按鈕的開關功能

---
+++ app/frontend/controllers/comment_controller.js
@@ -0,0 +1,27 @@
+// import { Controller } from "stimulus";
+
+// export default class extends Controller {
+//   static targets = [];
+
+//   check(e) {
+//     console.log(this.contentTarget.value);
+//     if (this.contentTarget.value.length >= 2) {
+//       console.log("ok");
+//     }
+//   }
+// }
+
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = ["content", "submit"];
+
+  check(e) {
+    if (this.contentTarget.value.length >= 2) {
+      console.log("ok");
+      this.submitTarget.disabled = false;
+    } else {
+      this.submitTarget.disabled = true;
+    }
+  }
+}
