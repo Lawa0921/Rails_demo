072f681b90044f6b79ac8eb4e913c5b3f7b02b81
Lawa0921 <bag571ivy3470@gmail.com>
2020-04-21 15:43:53 +0800

    我的最愛 AJAX 改用 Rails

--- app/frontend/controllers/item_controller.js
+++ app/frontend/controllers/item_controller.js
@@ -1,5 +1,7 @@
 import { Controller } from "stimulus";
-import axios from "axios";
+import Rails from "@rails/ujs";
+
+// import axios from "axios";
 
 export default class extends Controller {
   static targets = ["icon"];
@@ -11,35 +13,41 @@ export default class extends Controller {
   heart(e) {
     e.preventDefault();
 
-    const csrfToken = document.querySelector("[name=csrf-token]").content;
-    axios.defaults.headers.common["X-CSRF-TOKEN"] = csrfToken;
+    // const csrfToken = document.querySelector("[name=csrf-token]").content;
+    // axios.defaults.headers.common["X-CSRF-TOKEN"] = csrfToken;
 
     let item_id = document.querySelector("#item_id").value;
 
-    axios
-      .post(`/api/v1/items/${item_id}/favorite`)
-      .then((resp) => {
-        if (resp.data.status === "favorited") {
+    Rails.ajax({
+      url: `/api/v1/items/${item_id}/favorite`,
+      type: "POST",
+      success: (resp) => {
+        if (resp.status === "favorited") {
           this.iconTarget.classList.remove("far");
           this.iconTarget.classList.add("fas");
         } else {
           this.iconTarget.classList.remove("fas");
           this.iconTarget.classList.add("far");
         }
-      })
-      .catch(function (err) {
+      },
+      error: (err) => {
         console.log(err);
-      });
+      },
+    });
+
+    // axios
+    //   .post(`/api/v1/items/${item_id}/favorite`)
+    //   .then((resp) => {
+    //     if (resp.data.status === "favorited") {
+    //       this.iconTarget.classList.remove("far");
+    //       this.iconTarget.classList.add("fas");
+    //     } else {
+    //       this.iconTarget.classList.remove("fas");
+    //       this.iconTarget.classList.add("far");
+    //     }
+    //   })
+    //   .catch(function (err) {
+    //     console.log(err);
+    //   });
   }
 }
-
-// console.log("heart is clicked!");
-// if (this.clicked) {
-//   this.iconTarget.classList.remove("fas");
-//   this.iconTarget.classList.add("far");
-//   this.clicked = false;
-// } else {
-//   this.iconTarget.classList.remove("far");
-//   this.iconTarget.classList.add("fas");
-//   this.clicked = true;
-// }
