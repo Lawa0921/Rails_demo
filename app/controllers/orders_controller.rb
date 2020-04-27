a9d21f0e41bd0d563a1d726127387151adc7b662
Lawa0921 <bag571ivy3470@gmail.com>
2020-04-27 12:48:37 +0800

    新增購物車結帳部分功能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,35 @@
 class OrdersController < ApplicationController
+  def index
+  end
+
+  def create
+    # @order = Order.new(order_params)
+    # @order.user = current_user
+
+    @order = current_user.orders.build(order_params)
+
+    current_cart.items.each do |cart_item|
+      i = OrderItem.new(item: cart_item.item,
+                        price: cart_item.item.price,
+                        quantity: cart_item.quantity)
+      @order.order_items << i
+    end
+
+    if @order.save
+      redirect_to root_path, notice: 'ok'
+    else
+      redirect_to root_path, notice: 'not ok'
+    end
+  end
+
+  def show
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:receiver, :phone, :address)
+  end
+  def find_order
+    @order = Order.find(params[:id])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # attr_accessor :password_confirm
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, confirmation: true, length: {minimum: 4}
+  
   has_many :histories
   has_many :event, through: :histories
   has_many :comments
